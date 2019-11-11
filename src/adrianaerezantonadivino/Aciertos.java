c8877baffb1c4cd0aca2cd771e8c21c80f85a2ff
aperezan91 <aperezan91@gmail.com>
2019-11-11 20:10:02 +0100

    Números entre 10 y 50

--- src/adrianaerezantonadivino/Aciertos.java
+++ src/adrianaerezantonadivino/Aciertos.java
@@ -5,8 +5,8 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
-        //System.out.print(x);
+        int x = (int) (Math.random()*40 + 10);
+        System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
